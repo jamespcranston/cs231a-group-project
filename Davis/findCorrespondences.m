aa372c262c155f8c82258f00da75be9159b911ec
daviswert <daviswer@stanford.edu>
2016-05-12 23:49:34 -0700

    Sliding-window algorithm complete

---
+++ Davis/findCorrespondences.m
@@ -0,0 +1,50 @@
+# Takes 2 rectified images of the same dimension, and performs sliding-window
+# correspondence algorithm for disparity mapping. Uses normalized cross-
+# correlation and dual-aggregation harmonic mean to combine the colorbar
+# channels. Also takes a window diameter and threshold level between 0 and 1
+# representing stringency of correspondence points. 0 includes everything
+# and 1 accepts only perfect matches. 
+
+# Returns two nx2 matrices. Each row contains the pixel coordinates of a 
+# corresponding point in the rectified image. The first matrix corresponds
+# to the first image passed as argument, and the second to the second. 
+
+function [P1, P2] = findCorrespondences(IM1, IM2, diam, thresh)
+  offset = floor(diam/2);
+  w = size(IM1, 2);
+  h = size(IM1, 1);
+  P1 = zeros(0,2);
+  P2 = zeros(0,2);
+  # zero-pad the images
+  im1 = zeros(h+2*offset, w+2*offset, 3);
+  im2 = zeros(h+2*offset, w+2*offset, 3);
+  im1(1+offset:h+offset, 1+offset:w+offset, :) = IM1;
+  im2(1+offset:h+offset, 1+offset:w+offset, :) = IM2;
+  for i=1+offset:h+offset
+    for j1=1+offset:w+offset
+      # Create normalized window in image 1
+      w1 = reshape(im1(i-offset:i+offset,j1-offset:j1+offset,:),1,diam^2,3);
+      w1 = w1.-mean(w1,2);
+      w1 = w1./sqrt(sum(w1.^2,2));
+      best = 0;
+      bestcoord = 0;
+      for j2=1+offset:w+offset
+        # Create normalized window in image 2
+        w2 = reshape(im2(i-offset:i+offset,j2-offset:j2+offset,:),1,diam^2,3);
+        w2 = w2.-mean(w2,2);
+        w2 = w2./sqrt(sum(w2.^2,2));
+        # Compute normalized cross-correlation and dual-aggregation h-mean
+        scores = sum(w1.*w2,2);
+        score = 1-3/sum(1./(1.-scores));
+        if score > best
+          best = score;
+          bestcoord = j2;
+        end
+      end
+      if best > thresh
+        P1 = [P1;[i,j1]];
+        P2 = [P2;[i,bestcoord]];
+      end
+    end
+  end
+end
--- Davis/temp.m
+++ Davis/temp.m
@@ -1,8 +1,43 @@
-A = [ 
-p1(1)*M1(3,:) - M1(1,:);
-p1(2)*M1(3,:) - M1(2,:);
-p2(1)*M2(3,:) - M2(1,:);
-p2(2)*M2(3,:) - M2(2,:);
-]
-[U,S,V] = svd(A);
-X = V(:,4)./V(4,4);
+IM1 = 255.*rand(10,10,3);
+IM2 = 255.*rand(10,10,3);
+diam = 5;
+thresh = 0.9;
+
+
+offset = floor(diam/2);
+w = size(IM1, 2);
+h = size(IM1, 1);
+P1 = zeros(0,2);
+P2 = zeros(0,2);
+# zero-pad the images
+im1 = zeros(h+2*offset, w+2*offset, 3);
+im2 = zeros(h+2*offset, w+2*offset, 3);
+im1(1+offset:h+offset, 1+offset:w+offset, :) = IM1;
+im2(1+offset:h+offset, 1+offset:w+offset, :) = IM2;
+for i=1+offset:h+offset
+  for j1=1+offset:w+offset
+    # Create normalized window in image 1
+    w1 = reshape(im1(i-offset:i+offset,j1-offset:j1+offset,:),1,diam^2,3);
+    w1 = w1.-mean(w1,2);
+    w1 = w1./sqrt(sum(w1.^2,2));
+    best = 0;
+    bestcoord = 0;
+    for j2=1+offset:w+offset
+      # Create normalized window in image 2
+      w2 = reshape(im2(i-offset:i+offset,j2-offset:j2+offset,:),1,diam^2,3);
+      w2 = w2.-mean(w2,2);
+      w2 = w2./sqrt(sum(w2.^2,2));
+      # Compute normalized cross-correlation and dual-aggregation h-mean
+      scores = sum(w1.*w2,2);
+      score = 1-3/sum(1./(1.-scores));
+      if score > best
+        best = score;
+        bestcoord = j2;
+      end
+    end
+    if best > thresh
+      P1 = [P1;[i,j1]];
+      P2 = [P2;[i,bestcoord]];
+    end
+  end
+end
