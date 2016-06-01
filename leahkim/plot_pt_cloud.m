97194178eae9e4e8901c5c4cf7458a0fb47323cd
Lea Hyun Kim <leahkim@stanford.edu>
2016-05-31 22:28:36 -0700

    renamed triangulate and modified plotting

--- leahkim/plot_pt_cloud.m
+++ leahkim/plot_pt_cloud.m
@@ -1,10 +1,10 @@
-function plot_pt_cloud(X, Y, Z, C)
+function plot_pt_cloud(X, Y, Z, R, G, B)
     % X, Y, and Z should be vectors of the same length
-    % C should be in [R G B] format
+    % C should be in [R G B] format and between values 0 and 1
     %X = randi(100, 1, 100);
     %Y = randi(100, 1, 100);
     %Z = randi(100, 1, 100);
-    %C = [1 0.7 0.3]
     %figure('Point Cloud Result');
-    scatter3(X,Y,Z, 'MarkerFaceColor', C, 'MarkerEdgeColor', C);
+    C = [R G B]./255;
+    scatter3(X,Y,Z, 25, C, 'filled');
 end
